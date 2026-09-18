5d03584c80bf824b232be16e2b49bba3f980ecd4
Fábio S. M. Fernandes <fabiosoaresmoreirafernandesbjj@gmail.com>
2026-09-18 04:03:10 -0300

    Correção de ficheiros para o Vercel

--- Src/App.jsx
+++ Src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import AutenticacaoArboris from './componentes/AutenticacaoArboris/AutenticacaoArboris'; 
 
+
+
 function App() {
   return (
     <Routes>
